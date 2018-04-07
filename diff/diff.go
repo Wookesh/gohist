120996b9ae16c4621db30361726617bba9a65e6c
Łukasz Piesiewicz <wookesh@gmail.com>
2018-04-07 17:31:55 +0200

    removed debug messages

--- diff/diff.go
+++ diff/diff.go
@@ -31,7 +31,6 @@ func LCS(a, b string, offset int, mode Mode) (coloring Coloring) {
 	bList := strings.Split(b, "\n")
 	c := lcs(aList, bList)
 	for _, simpleColor := range printLCS(c, aList, bList, len(aList), len(bList), mode) {
-		logrus.Infoln(simpleColor.Color, simpleColor.Data)
 		coloring = append(coloring, ColorChange{Color: simpleColor.Color, Pos: token.Pos(offset), End: token.Pos(offset + len(simpleColor.Data))})
 		offset += len(simpleColor.Data) + 1
 	}
@@ -39,23 +38,22 @@ func LCS(a, b string, offset int, mode Mode) (coloring Coloring) {
 }
 
 func printLCS(c [][]int, x, y []string, i, j int, mode Mode) (coloring []simpleColoring) {
-	logrus.Infoln(i, j, len(x), len(y))
 	if i > 0 && j > 0 && x[i-1] == y[j-1] {
 		coloring = printLCS(c, x, y, i-1, j-1, mode)
 		coloring = append(coloring, simpleColoring{Color: ColorSame, Data: x[i-1]})
-		logrus.Infoln(" " + x[i-1])
+		logrus.Debugln(" " + x[i-1])
 	} else {
 		if j > 0 && (i == 0 || c[i][j-1] >= c[i-1][j]) {
 			coloring = printLCS(c, x, y, i, j-1, mode)
 			if mode == ModeNew {
 				coloring = append(coloring, simpleColoring{Color: ColorNew, Data: y[j-1]})
-				logrus.Infoln("+" + y[j-1])
+				logrus.Debugln("+" + y[j-1])
 			}
 		} else if i > 0 && (j == 0 || c[i][j-1] < c[i-1][j]) {
 			coloring = printLCS(c, x, y, i-1, j, mode)
 			if mode == ModeOld {
 				coloring = append(coloring, simpleColoring{Color: ColorRemoved, Data: x[i-1]})
-				logrus.Infoln("-" + x[i-1])
+				logrus.Debugln("-" + x[i-1])
 			}
 		}
 	}
